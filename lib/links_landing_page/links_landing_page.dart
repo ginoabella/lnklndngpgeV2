a0298cc97dda60a04b9465f2ba90d2ad6fe73bb9
gino abella <gino@ginos-MacBook-Air.local>
2020-10-15 19:24:11 +0800

    third part

--- lib/links_landing_page/links_landing_page.dart
+++ lib/links_landing_page/links_landing_page.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:lnk_lndng_pge_v2/links_landing_page/button_link.dart';
 import 'package:lnk_lndng_pge_v2/constant.dart';
 import 'package:lnk_lndng_pge_v2/links_landing_page/footer.dart';
+import 'package:lnk_lndng_pge_v2/model/link_data.dart';
+import 'package:provider/provider.dart';
 
 class LinksLandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _documents = Provider.of<List<LinkData>>(context);
+    if (_documents == null) {
+      return Center(child: CircularProgressIndicator());
+    }
     return Material(
       child: Column(
         children: [
@@ -24,7 +30,12 @@ class LinksLandingPage extends StatelessWidget {
             ),
           ),
           SizedBox(height: 20),
-          for (var document in documents)
+          if (_documents.isEmpty)
+            Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Text('No Links yet to Display'),
+            ),
+          for (var document in _documents)
             ButtonLink(
               title: document.title,
               url: document.url,
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:lnk_lndng_pge_v2/links_landing_page/links_landing_page.dart';
+import 'package:lnk_lndng_pge_v2/model/link_data.dart';
 import 'package:lnk_lndng_pge_v2/not_found_page.dart';
 import 'package:lnk_lndng_pge_v2/settings_page/settings_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseFirestore.instance.settings = Settings(
+    host: 'localhost:8080',
+    sslEnabled: false,
+    persistenceEnabled: false,
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Link Landing Page',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    final linksCollection = FirebaseFirestore.instance.collection('links');
+    final userLinkDataString = linksCollection.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return LinkData.fromMap(doc.data());
+      }).toList();
+    });
+
+    return StreamProvider<List<LinkData>>(
+      create: (context) => userLinkDataString,
+      child: MaterialApp(
+        title: 'Link Landing Page',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LinksLandingPage(),
+          '/settings': (context) => SettingsPage(),
+        },
+        onUnknownRoute: (settings) {
+          return MaterialPageRoute(builder: (context) {
+            return NotFoundPage();
+          });
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LinksLandingPage(),
-        '/settings': (context) => SettingsPage(),
-      },
-      onUnknownRoute: (settings) {
-        return MaterialPageRoute(builder: (context) {
-          return NotFoundPage();
-        });
-      },
     );
   }
 }
